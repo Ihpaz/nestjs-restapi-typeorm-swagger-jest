e98dbdbc3e8de9a3c7f84201d5269de8a57113be
solusi asia informatika solace <ihfazm@gmail.com>
2023-03-09 14:19:10 +0700

    update testing

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -4,10 +4,20 @@ import { DatabaseModule } from '../config/database/database.module';
 import { AuthModule } from '../auth/auth.module';
 import { BooksService } from './books.service';
 import { Book } from './entities/book.entity';
+import { BookDto } from './dto/book.dto';
 
 describe('BooksService', () => {
   let service: BooksService;
-
+  let dto:BookDto= {
+    "Author": "Ihpaz",
+    "Title": "Fiqih",
+    "Ayod": 1289,
+    "Tags": [
+      "FiQIH",
+      "Sunnah"
+    ],
+    "Publisher": "Pustaka Sunnah"
+  }
   
 
   beforeEach(async () => {
@@ -25,4 +35,13 @@ describe('BooksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // it('should be error', async () => {
+    
+  //   try {
+  //       await service.create(dto);
+  //     } catch (e) {
+  //       expect(e.message).toMatch('Function is disable');
+  //     }
+  // });
 });
